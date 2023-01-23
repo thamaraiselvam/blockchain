36c973d59e22a8dfaf21b3c427319eaf79a22d0b
tom <thamaraiselvam@live.com>
2023-01-23 22:47:30 +0800

    Add new transaction and return current block

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,10 @@
 class Blockchain(object):
     def __init__(self):
+        # Maintain chain state
         self.chain = []
+        # Holds transactions for each block
         self.current_transactions = []
+        # Represents all connected nodes
         self.nodes = set()
 
     def new_block(self, proof, previous_hash=None):
@@ -10,7 +13,12 @@ class Blockchain(object):
 
     def new_transaction(self, sender, recipient, amount):
         # Adds a new transaction to the list of transactions
-        pass
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+        return self.last_block['index'] + 1
 
     @staticmethod
     def hash(block):
@@ -20,4 +28,4 @@ class Blockchain(object):
     @property
     def last_block(self):
         # Returns the last Block in the chain
-        pass
+        return self.chain[-1]
