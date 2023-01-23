c4ee616002ddfeabf61f2b0b3e0465f8a772a38c
tom <thamaraiselvam@live.com>
2023-01-23 22:52:22 +0800

    Run service on defined port

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,9 @@
 from time import time
+from uuid import uuid4
+from flask import Flask, jsonify, request
+import hashlib
+import json
+import os
 
 class Blockchain(object):
     def __init__(self):
@@ -35,9 +40,36 @@ class Blockchain(object):
     @staticmethod
     def hash(block):
         # Hashes a Block
-        pass
+        block_string = json.dumps(block,sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
         # Returns the last Block in the chain
         return self.chain[-1]
+
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identfier = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT')
+    print(port)
+    app.run(host='0.0.0.0', port=port)
